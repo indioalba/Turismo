5bdc13560169230ba2b84a7309885b3f7541e88c
indoialba <indioalba@gmail.com>
2016-03-05 18:50:43 +0000

    Initial Commit

---
+++ Turismo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Turismo
+//
+//  Created by Manuel Reyes on 5/3/16.
+//  Copyright © 2016 Manuel Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
